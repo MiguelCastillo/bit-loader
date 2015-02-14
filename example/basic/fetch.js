5009dcb2fc7c99dc2ac8668a5470b8bc8e2b710a
Miguel Castillo <manchagnu@gmail.com>
2015-02-14 11:17:54 -0500

    Adding running examples for register, fetch, and transforms

---
+++ example/basic/fetch.js
@@ -0,0 +1,28 @@
+var Bitloader = require('../../dist/bit-loader.js');
+var loader = new Bitloader({}, {fetch: fetchFactory});
+
+loader.import(["like1", "like2"]).then(function(r1, r2) {
+  console.log(r1, r2);
+});
+
+
+// When fetchFactory is called, the instance of loader is passed in.
+function fetchFactory(/*loader*/) {
+  function compile() {
+    console.log("compile '" + this.name + "'");
+    // `this` is an augmented meta module object that has access to manager,
+    // which is the instance of loader.
+    return new this.manager.Module({code: this.name + " is compiled"});
+  }
+
+  return {
+    fetch: function(name) {
+      console.log("fetching '" + name + "'");
+      // Notice that fetch returns a simple object with a `compile` method.
+      // When a `compile` method is provided, a `source` property of type
+      // string must also be proivded.
+      // This object returned is what we call a module meta object.
+      return {compile: compile, source: ""};
+    }
+  };
+}
