32833b4cc2893751055a8ec01d8664c607f0964b
Miguel Castillo <mcastillo@symphono.com>
2015-05-21 12:52:41 -0400

    changed compiler and resolver interface to have default behavior rather than throw exceptions if not implemented

--- src/interfaces/resolver.js
+++ src/interfaces/resolver.js
@@ -4,8 +4,11 @@
   function Resolver() {
   }
 
-  Resolver.prototype.resolve = function(/*moduleMeta, moduleParent*/) {
-    throw new TypeError("Resolver:resolve is not implemented, must be implemented by the consumer code");
+  Resolver.prototype.resolve = function(moduleMeta /*, moduleParent*/) {
+    return {
+      cname: moduleMeta.path,
+      path: moduleMeta.name
+    };
   };
 
   module.exports = Resolver;
