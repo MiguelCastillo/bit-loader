51b2048d9f98295063d9ddd54078b7c90f22b990
Miguel Castillo <manchagnu@gmail.com>
2015-06-20 12:51:08 -0400

    removed minimatch from unit test setup

--- test/config.js
+++ test/config.js
@@ -4,14 +4,12 @@ var require = (function() {
   var importer = bitimports.config({
     "baseUrl": "../",
     "paths": {
-      "chai": "node_modules/chai/chai",
-      "minimatch": "node_modules/minimatch/browser"
+      "chai": "node_modules/chai/chai"
     }
   });
 
-  importer.ignore({
-    match: ["chai", "minimatch", "dist/bit-loader"]
-  });
+  // Add modules to exclude from pipeline processing
+  importer.ignore(["chai", "dist/bit-loader"]);
 
   bitimports.Logger.enableAll();
   return importer.require;
