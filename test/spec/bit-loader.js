b9c4e7d3735b66857f50fa3d7a02d598d08370e0
Miguel Castillo <manchagnu@gmail.com>
2016-02-15 19:32:34 -0500

    Added ability to specify more complex objects similar to plugins

--- test/spec/bit-loader.js
+++ test/spec/bit-loader.js
@@ -11,7 +11,6 @@ describe("Bitloader Test Suite", function() {
     expect(bitloader instanceof(Bitloader)).to.equal(true);
   });
 
-
   describe("and registering a plugin with handlers for `fetch`, `transform`, `dependency`, and `compile`", function() {
     var defaultResolveStub, defaultFetchStub, defaultCompileStub, resolveStub, fetchStub, transformStub, dependencyStub, compileStub;
 
@@ -26,60 +25,129 @@ describe("Bitloader Test Suite", function() {
       compileStub        = sinon.spy(function() { return { exports: "compiled source" }; });
 
       bitloader = new Bitloader({
-        resolve : defaultResolveStub,
-        fetch   : defaultFetchStub,
-        compile : defaultCompileStub
+          resolve : defaultResolveStub,
+          fetch   : defaultFetchStub,
+          compile : defaultCompileStub
+        })
+        .plugin({
+          resolve    : resolveStub,
+          fetch      : fetchStub,
+          transform  : transformStub,
+          dependency : dependencyStub,
+          compile    : compileStub
+        });
+    });
+
+
+    describe("and importing a module called `js`", function() {
+      beforeEach(function() {
+        return bitloader.import("js");
       });
 
-      bitloader.plugin({
-        resolve    : resolveStub,
-        fetch      : fetchStub,
-        transform  : transformStub,
-        dependency : dependencyStub,
-        compile    : compileStub
+      it("then default 'fetch' is NOT called", function() {
+        sinon.assert.notCalled(defaultFetchStub);
       });
 
-      return bitloader.import("js");
+      it("then default 'compile' is NOT called", function() {
+        sinon.assert.notCalled(defaultCompileStub);
+      });
+
+      it("then default `resolve` is NOT called", function() {
+        sinon.assert.notCalled(defaultResolveStub);
+      });
+
+      it("then `resolve` plugin is called once", function() {
+        sinon.assert.calledOnce(resolveStub);
+      });
+
+      it("then `resolve` plugin is called with a module meta with name `js`", function() {
+        sinon.assert.calledWith(resolveStub, sinon.match({name: "js"}));
+      });
+
+      it("then `fetch` plugin is called once", function() {
+        sinon.assert.calledOnce(fetchStub);
+      });
+
+      it("then `fetch` plugin is called with a module meta with path `some/path`", function() {
+        sinon.assert.calledWith(fetchStub, sinon.match({path: "some/path"}));
+      });
+
+      it("then `transform` plugin is called once", function() {
+        sinon.assert.calledOnce(transformStub);
+      });
+
+      it("then `dependency` plugin is called once", function() {
+        sinon.assert.calledOnce(dependencyStub);
+      });
+
+      it("then `compile` plugin is NOT called", function() {
+        sinon.assert.notCalled(compileStub);
+      });
     });
 
-    it("then default 'fetch' is NOT called", function() {
-      sinon.assert.notCalled(defaultFetchStub);
+    describe("and adding a single string `ignore` rule", function() {
+      var rule;
+
+      beforeEach(function() {
+        rule = "lagunita";
+        bitloader.services.transform.ignore = sinon.stub();
+        bitloader.ignore(rule);
+      });
+
+      it("then `transform` service is called with `name` `laguna`", function() {
+        sinon.assert.calledWithExactly(bitloader.services.transform.ignore, "name", "lagunita");
+      });
     });
 
-    it("then default 'compile' is NOT called", function() {
-      sinon.assert.notCalled(defaultCompileStub);
+    describe("and adding a string array of `ignore` rules", function() {
+      var rule;
+
+      beforeEach(function() {
+        rule = ["lagunita", "robot", "chicken"];
+        sinon.stub(bitloader.services.transform, "ignore");
+        bitloader.ignore(rule);
+      });
+
+      it("then `transform` service is called with `name` `laguna`", function() {
+        sinon.assert.calledWithExactly(bitloader.services.transform.ignore, "name", "lagunita");
+      });
+
+      it("then `transform` service is called with `name` `robot`", function() {
+        sinon.assert.calledWithExactly(bitloader.services.transform.ignore, "name", "robot");
+      });
+
+      it("then `transform` service is called with `name` `chicken`", function() {
+        sinon.assert.calledWithExactly(bitloader.services.transform.ignore, "name", "chicken");
+      });
     });
 
-    it("then default `resolve` is NOT called", function() {
-      sinon.assert.notCalled(defaultResolveStub);
-    });
+    describe("and adding an array of `ignore` rules with a `name`, a `path`, and `magic` properties", function() {
+      var rules;
 
-    it("then `resolve` plugin is called once", function() {
-      sinon.assert.calledOnce(resolveStub);
-    });
+      beforeEach(function() {
+        rules = [{
+          name: "lagunita"
+        }, {
+          path: "robot"
+        }, {
+          magic: /chicken/
+        }];
 
-    it("then `resolve` plugin is called with a module meta with name `js`", function() {
-      sinon.assert.calledWith(resolveStub, sinon.match({name: "js"}));
-    });
+        sinon.stub(bitloader.services.transform, "ignore");
+        bitloader.ignore(rules);
+      });
 
-    it("then `fetch` plugin is called once", function() {
-      sinon.assert.calledOnce(fetchStub);
-    });
+      it("then `transform` service is called with `name` `laguna`", function() {
+        sinon.assert.calledWithExactly(bitloader.services.transform.ignore, "name", "lagunita");
+      });
 
-    it("then `fetch` plugin is called with a module meta with path `some/path`", function() {
-      sinon.assert.calledWith(fetchStub, sinon.match({path: "some/path"}));
-    });
+      it("then `transform` service is called with `path` `robot`", function() {
+        sinon.assert.calledWithExactly(bitloader.services.transform.ignore, "path", "robot");
+      });
 
-    it("then `transform` plugin is called once", function() {
-      sinon.assert.calledOnce(transformStub);
-    });
-
-    it("then `dependency` plugin is called once", function() {
-      sinon.assert.calledOnce(dependencyStub);
-    });
-
-    it("then `compile` plugin is NOT called", function() {
-      sinon.assert.notCalled(compileStub);
+      it("then `transform` service is called with `magic` /chicken/", function() {
+        sinon.assert.calledWithExactly(bitloader.services.transform.ignore, "magic", /chicken/);
+      });
     });
   });
 });
